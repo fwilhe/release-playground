32bfa89f4120978b10a0e36ffb3882221659564e
Florian Wilhelm <florian.wilhelm02@sap.com>
2023-12-18 13:32:09 +0100

    improve

--- .github/workflows/bump.py
+++ .github/workflows/bump.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 
+"""
+Determine next version number for versions of a schema like v1.0
+based on existing git tags and which component to bump (minor/major).
+"""
+
 import subprocess
 import re
 import sys
 import os
 
+
 def convert_version_to_sortable_int(major, minor):
     return major * 1000 + minor
+
 
 def determine_most_recent_existing_version():
     tags = subprocess.run(["git", "tag"], capture_output=True).stdout.splitlines()
@@ -19,7 +26,6 @@ def determine_most_recent_existing_version():
             tag_without_prefix = tag[1:]
             components = tag_without_prefix.split(".")
             assert len(components) == 2
-            print(components)
             major_int = int(components[0])
             minor_int = int(components[1])
             versions.append(
@@ -31,12 +37,23 @@ def determine_most_recent_existing_version():
                 }
             )
 
-    def key(v):
+    if len(versions) == 0:
+        print("No existing versions found")
+        return {
+            "tag": "v0.0",
+            "sortNumber": convert_version_to_sortable_int(0, 0),
+            "major": 0,
+            "minor": 0,
+        }
+
+    def keyToSortVersions(v):
         return v["sortNumber"]
 
-    versions.sort(key=key, reverse=True)
-    print(versions)
-    return versions[0]
+    versions.sort(key=keyToSortVersions, reverse=True)
+    print(f"Sorted list of versions: {versions}")
+    highest_existing_version_number = versions[0]
+
+    return highest_existing_version_number
 
 
 def bump(most_recent_version, component_to_bump):
@@ -50,7 +67,9 @@ def bump(most_recent_version, component_to_bump):
         major = most_recent_version["major"]
         new_version = f"v{major}.{new_minor}"
     else:
-        raise (f'Invalid component provided: {component_to_bump}, only major or minor are supported.')
+        raise (
+            f"Invalid component provided: {component_to_bump}, only major or minor are supported."
+        )
 
     return new_version
 
